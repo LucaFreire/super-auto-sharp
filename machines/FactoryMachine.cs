fd4fecdfac9cbcec294e774e2262a9769042c2de
fortunato-sply <lucasfortunato2530@gmail.com>
2023-05-25 08:20:27 -0300

    update: factoryMachine finished

--- machines/FactoryMachine.cs
+++ machines/FactoryMachine.cs
@@ -9,14 +9,36 @@ public class FactoryMachine : FactoryMachineMethod
 {
     public Machine Create(string name)
     {
-        switch(name)
+        switch(name.ToLower())
         {
-            case "Martelo":
+            case "martelo": // tier 1
                 return new Martelo();
-            case "Chave de Fenda":
+            case "chave de fenda":
                 return new ChaveDeFenda();
-            case "Esteira":
+            case "esteira":
                 return new Esteira();
+            case "forno industrial a gas": // tier 2
+                return new FornoIndustrialAGas();
+            case "furadeira de coluna":
+                return new FuradeiraDeColuna();
+            case "retifica plana":
+                return new RetificaPlana();
+            case "forno industrial eletrico": // tier 3
+                return new FornoIndustrialEletrico();
+            case "furadeira de coordenada":
+                return new FuradeiraDeCoordenada();
+            case "retifica cilindrica":
+                return new RetificaCilindrica();
+            case "fresa":  // tier 4
+                return new Fresa();
+            case "torno":
+                return new Torno();
+            case "fresa cnc": // tier 5
+                return new FresaCNC();
+            case "torno cnc":
+                return new TornoCNC();
+            case "corte a plasma cnc":
+                return new CorteAPlasmaCNC(); // tier 6
             default:
                 throw new Exception("Máquina inválida.");
         }
