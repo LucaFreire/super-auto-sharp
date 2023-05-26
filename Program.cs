b4f682841d3ef47b92a71eeaa9d970dc29f69a01
LucaFreire <lucasrodifreire@gmail.com>
2023-05-26 08:22:40 -0300

    Fixing: ShowStock

--- Program.cs
+++ Program.cs
@@ -4,17 +4,22 @@ class Program
 {
     static void Main()
     {
-        FactoryMachine factory = new FactoryMachine();
-        var list = factory.CreateMachines();
-        foreach(Machine mac in list)
-        {
-            Console.WriteLine(mac);
-        }
+        // FactoryMachine factory = new FactoryMachine();
+        // var list = factory.CreateMachines();
+        // foreach(Machine mac in list)
+        // {
+        //     Console.WriteLine(mac);
+        // }
 
-        var listt = RandomMachines.GetRandomMachines();
-        foreach(Machine mac in listt)
-        {
-            Console.WriteLine(mac);
-        }
+        // var listt = RandomMachines.GetRandomMachines();
+        // foreach(Machine mac in listt)
+        // {
+        //     Console.WriteLine(mac);
+        // }
+    
+        Shop sh = new Shop();
+
+        sh.ShowStock();
+    
     }
 }
--- Shop/Shop.cs
+++ Shop/Shop.cs
@@ -2,11 +2,18 @@ namespace SAS;
 public class Shop
 {
     public static List<Machine> stock = new List<Machine>();
-    private void GetStock()
+    private void getStock(bool clear = false)
     {
+        if(clear)
+            stock.Clear();
         List<Machine> newStock = new List<Machine>();
         newStock = RandomMachines.GetRandomMachines();
         stock = newStock;
     }
-
+    public void ShowStock()
+    {
+        getStock();
+        foreach (var item in stock)
+            Console.WriteLine($"{item.Name}: {item.Atk} / {item.Def}");
+    }
 }
